26cd8a0cd179be5050ab0cd601567ee5dd1f710d
menezesmalu <mlmv@cin.ufpe.br>
2017-11-30 09:36:22 -0300

    [#153180593] implementação do teste classe de cadastrar produto com sucesso

---
+++ server/ap-server/spec/cadastrodeproduto.spec.ts
@@ -0,0 +1,32 @@
+import { CadastroDeProdutos } from '../cadastrodeproduto';
+import { Produto } from '../../../gui/ap-gui/src/app/produto';
+
+describe("O cadastro de alunos", () => {
+  var cadastro: CadastroDeAlunos;
+
+  beforeEach(() => cadastro = new CadastroDeAlunos())
+
+  it("é inicialmente vazio", () => {
+    expect(cadastro.getProdutos().length).toBe(0);
+  })
+
+  it("cadastra produtos corretamente", () => {
+    var prod: Produto = new Produto();
+    prod.codigo = '001'
+    prod.nome = "camarao cinza";
+    prod.valor = '33';
+    prod.unid = "kg";
+    prod.categoria = "camarões"
+    cadastro.criar(prod);
+
+    expect(cadastro.getProdutos().length).toBe(1);
+    prod = cadastro.getProdutos()[0];
+    expect(prod.codigo).toBe("001");
+    expect(prod.nome).toBe("camao cinza");
+    expect(prod.valor).toBe("33");
+    expect(prod.unid).toBe("kg");
+    expect(prod.categoria).toBe("camaroes");
+  })
+
+
+})
