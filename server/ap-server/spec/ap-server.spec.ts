6a60a3a02f43a9b85062bf9c43c355953e73ee95
Rafael <rps4@cin.ufpe.br>
2017-12-01 00:26:28 -0300

    [#153182313]Implementação do teste de servidor para pedidos pendentes

---
+++ server/ap-server/spec/ap-server.spec.ts
@@ -0,0 +1,27 @@
+import request = require("request-promise");
+import { closeServer } from '../ap-server';
+
+var base_url = "http://localhost:3000/";
+
+describe("O servidor", () => {
+  var server:any;
+
+  beforeAll(() => {server = require('../ap-server')});
+
+  afterAll(() => {server.closeServer()});
+
+  it("inicialmente retorna uma lista de alunos vazia", () => {
+    return request.get(base_url + "pedidos").then(body => expect(body).toBe("[]")).catch(e => expect(e).toEqual(null));
+  })
+
+  it("não cadastra alunos com CPF duplicado", () => {
+    return request.post(base_url + "pedidos", {"json":{"cliente": {"nome": "Paulo"}, "entregue" : false, "pago": false}}).then(body => {
+         expect(body).toEqual({success: "O aluno foi cadastrado com sucesso"});
+             expect(body).toEqual({failure: "O aluno não pode ser cadastrado"});
+             return request.get(base_url + "alunos").then(body => {
+                 expect(body).toContain('{"cliente": {"nome": "Paulo"}, "entregue" : false, "pago": false}');
+             });
+     });
+  })
+
+})
