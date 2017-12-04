fc7768b7614cb784aa4a59b05153c33e7ef03ab8
Igor de Andrade Farias Poncell <iafp@WINDOWS.CIN.UFPE.BR>
2017-12-04 09:30:53 -0300

    [#153283338] Teste de aceitacao alterar local de retirada

--- tests-acceptance/stepdefinitions/visualizarpedido.ts
+++ tests-acceptance/stepdefinitions/visualizarpedido.ts
@@ -98,4 +98,75 @@ defineSupportCode(function ({ Given, When, Then }) {
     });
 
 })
+
+
+    Given(/^eu estou na página do pedido$/, async () => {
+        await browser.get("http://localhost:4200/");
+        await expect(browser.getTitle()).to.eventually.equal('ApGui');
+    })
+
+    Given(/^eu visualizo o campo “Nome” com "([^\"]*)"$/, async (client) => {
+        var idcampo = "labelNome"
+        var campo = document.getElementById("idcampo");
+        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal('Gustavo Augusto');
+    });
+
+    Given(/^eu visualizo o campo “Telefone” com "([^\"]*)"$/, async (client) => {
+        var idcampo = "labelTelefone"
+        var campo = document.getElementById("idcampo");
+        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal('998765432');
+    });
+
+    Given(/^eu visualizo o campo “Data pedido” com "([^\"]*)"$/, async (client) => {
+        var idcampo = "labelDataPedido"
+        var campo = document.getElementById("idcampo");
+        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal('07/11/2017');
+    });
+
+    Given(/^eu visualizo o campo “Data entrega” com "([^\"]*)"$/, async (client) => {
+        var idcampo = "labelDataEntrega"
+        var campo = document.getElementById("idcampo");
+        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal('10/11/2017');
+    });
+
+    Given(/^eu visualizo o campo “Local de retirada” com "([^\"]*)"$/, async (client) => {
+        var idcampo = "inputLocalRetirada"
+        var campo = document.getElementById("idcampo");
+        expect($(input[id=“ + idcampo + ”]”).getText()).to.eventually.equal('A peixaria');
+    });
+
+    Given(/^eu visualizo o campo “Desconto” com "([^\"]*)"$/, async (client) => {
+        var idcampo = "labelDesconto"
+        var campo = document.getElementById("idcampo");
+        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal('15');
+    });
+
+    Given(/^eu visualizo o campo “Tipo de desconto” com "([^\"]*)"$/, async (client) => {
+        var idcampo = "labelTipoDesconto"
+        var campo = document.getElementById("idcampo");
+        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal('%');
+    });
+
+    Given(/^eu visualizo o campo “Condição de pagamento” com "([^\"]*)"$/, async (client) => {
+        var idcampo = "labelCondiçãoPagamento"
+        var campo = document.getElementById("idcampo");
+        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal('À vista');
+    });
+
+    Given(/^eu visualizo o campo “Quantidade” com "([^\"]*)"$/, async (client) => {
+        var idcampo = "labelQuantidade"
+        var campo = document.getElementById("idcampo");
+        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal('5');
+
+    });
+
+    When(/^eu edito o campo “local de retirada” com "([^\"]*)”$/, async (localRetirada) => {
+        await $("input[id='inputLocalRetirada']").sendKeys(<string> localRetirada);
+    });
     
+    Then(/^o local de retirada é alterado para "([^\"]*)"$/, async (localRetirada) => {
+        var idcampo = "inputLocalRetirada"
+        var campo = document.getElementById("idcampo");
+        expect($(input[id=“ + idcampo + ”]”).getText()).to.eventually.equal(<string> localRetirada);
+    });
+})
