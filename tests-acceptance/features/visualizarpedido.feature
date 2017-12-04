05c1be3b5d8780b0f74f6c6dc3566713ccf21e4c
Igor de Andrade Farias Poncell <iafp@WINDOWS.CIN.UFPE.BR>
2017-12-04 09:38:38 -0300

    Ajustes nos cenarios e testes

--- tests-acceptance/features/visualizarpedido.feature
+++ tests-acceptance/features/visualizarpedido.feature
@@ -14,14 +14,14 @@ And	eu visualizo o campo “Desconto” com “15”
 And	eu visualizo o campo “Tipo de desconto” com “%”
 And	eu visualizo o campo “Condição de pagamento” com “À vista”
 And	eu visualizo o campo “Quantidade” com “5”
-And	eu visualizo o campo uma lista com todos os produtos cadastrados
-And	eu visualizo o botão de marcar como “pago”
-And	eu visualizo o botão de marcar como “entregue”
-And	eu visualizo o botão de marcar como “cancelado”
-When	eu clico no botão de marcar como “pago”
-And	eu clico no botão de marcar como “entregue”
-Then 	o pedido recebe a label de “pago”
-And   	o pedido recebe a label de “entregue”
+And	eu visualizo a label da lista com todos os produtos cadastrados
+And	eu visualizo o checkbox de marcar como pago
+And	eu visualizo o checkbox de marcar como entregue
+And	eu visualizo o checkbox de marcar como cancelado
+When eu clico no checkbox de marcar como pago
+And	eu clico no checkbox de marcar como entregue
+Then 	o pedido recebe a label de pago
+And   	o pedido recebe a label de entregue
 
 Scenario: Alterar local de retirada
 Given eu estou na página do pedido
@@ -34,10 +34,7 @@ And	eu visualizo o campo “Desconto” com “15”
 And	eu visualizo o campo “Tipo de desconto” com “%”
 And	eu visualizo o campo “Condição de pagamento” com “À vista”
 And	eu visualizo o campo “Quantidade” com “5”
-And	eu visualizo o campo uma lista com todos os produtos cadastrados
-And	eu visualizo o botão de marcar como “pago”
-And	eu visualizo o botão de marcar como “entregue”
-And	eu visualizo o botão de marcar como “cancelado”
+And	eu visualizo a label da lista com todos os produtos cadastrados
 When	eu edito o campo “local de retirada” com “Rua bione, 220”
 Then 	o local de retirada é alterado para “Rua bione, 220”
 
@@ -53,8 +50,8 @@ And	eu visualizo o campo “Tipo de desconto” com “%”
 And	eu visualizo o campo “Condição de pagamento” com “À vista”
 And	eu visualizo o campo “Quantidade” com “5”
 And	eu visualizo o campo uma lista com todos os produtos cadastrados
-And	eu visualizo o botão de marcar como “pago”
-And	eu visualizo o botão de marcar como “entregue”
-And	eu visualizo o botão de marcar como “cancelado”
-When	eu clico no botão de marcar como “cancelar”
-Then 	o pedido recebe a label de “cancelar”
+And	eu visualizo o checkbox de marcar como pago
+And	eu visualizo o checkbox de marcar como entregue
+And	eu visualizo o checkbox de marcar como cancelado
+When eu clico no checkbox de marcar como cancelado
+Then o pedido estara marcado como cancelado
--- tests-acceptance/stepdefinitions/visualizarpedido.ts
+++ tests-acceptance/stepdefinitions/visualizarpedido.ts
@@ -67,95 +67,120 @@ defineSupportCode(function ({ Given, When, Then }) {
 
     });
 
-    Given(/^eu visualizo o botão de marcar como pago$/, async () => {
-        var idElem = "botaoPago"
+    Given(/^eu visualizo a label da lista com todos os produtos cadastrados$/, async () => {
+        var idcampo = "labelListaProdutos"
+        var campo = document.getElementById("idcampo");
+        expect($(“label[id=“ + idcampo + ”]”).isPresent());
+    });
+
+    Given(/^eu visualizo o checkbox de marcar como  pago$/, async () => {
+        var idElem = "checkboxPago"
         var elem = document.getElementById("idElem");
-        expect($(“button[id=“ + idElem+ ”]”).getText()).to.eventually.equal('Pago');
+        expect($(“input[id=“ + idElem+ ”]”).isPresent());
     });
 
     Given(/^eu visualizo o botão de marcar como entregue$/, async () => {
-        var idElem = "botaoEntregue"
+        var idElem = "checkboxEntregue"
         var elem = document.getElementById("idElem");
-        expect($(“button[id=“ + idElem+ ”]”).getText()).to.eventually.equal('Entregue');
+        expect($(“input[id=“ + idElem+ ”]”).isPresent());
+
+    });
+    Given(/^eu visualizo o botão de marcar como cancelado$/, async () => {
+        var idElem = "checkboxCancelado"
+        var elem = document.getElementById("idElem");
+        expect($(“input[id=“ + idElem+ ”]”).isPresent());
 
     });
 
-    When(/^eu clico no botão de marcar como “entregue”$/, async () => {
-       await element(by.buttonText('“Entregue”')).click();
-    });
-    When(/^eu clico no botão de marcar como “pago”$/, async () => {
-       await element(by.buttonText('“Pago”')).click();
+    When(/^eu clico no checkbox de marcar como “entregue”$/, async () => {
+       await element(by.id('“Entregue”')).click();
     });
 
-    Then(/^o pedido estara marcado como entregue e pago$/, async () => {
+    When(/^eu clico no checkbox de marcar como “pago”$/, async () => {
+       await element(by.input('“Pago”')).click();
+    });
+
+    Then(/^o pedido recebe a label de pago$/, async () => {
         var idLabel1 = "labelPago"
         var label1 = document.getElementById("idLabel1");
         expect($(“label[id=“ + idLabel1 + ”]”).isPresent());
+
+    });
+    Then(/^o pedido recebe a label de entregue$/, async () => {
         var idLabel2 = "labelEntregue"
         var label2 = document.getElementById("idLabel2");
         expect($(“label[id=“ + idLabel2 + ”]”).isPresent());
 
     });
 
+    
+
     Given(/^eu estou na página do pedido$/, async () => {
         await browser.get("http://localhost:4200/");
         await expect(browser.getTitle()).to.eventually.equal('ApGui');
     })
 
-    Given(/^eu visualizo o campo “Nome” com "([^\"]*)"$/, async (client) => {
+    Given(/^eu visualizo o campo “Nome” com "([^\"]*)"$/, async (nome) => {
         var idcampo = "labelNome"
         var campo = document.getElementById("idcampo");
-        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal('Gustavo Augusto');
+        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal(<string> nome);
     });
 
-    Given(/^eu visualizo o campo “Telefone” com "([^\"]*)"$/, async (client) => {
+    Given(/^eu visualizo o campo “Telefone” com "([^\"]*)"$/, async (telefone) => {
         var idcampo = "labelTelefone"
         var campo = document.getElementById("idcampo");
-        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal('998765432');
+        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal(<string> telefone);
     });
 
-    Given(/^eu visualizo o campo “Data pedido” com "([^\"]*)"$/, async (client) => {
+    Given(/^eu visualizo o campo “Data pedido” com "([^\"]*)"$/, async (dataPedido) => {
         var idcampo = "labelDataPedido"
         var campo = document.getElementById("idcampo");
-        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal('07/11/2017');
+        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal(<string> dataPedido);
     });
 
-    Given(/^eu visualizo o campo “Data entrega” com "([^\"]*)"$/, async (client) => {
+    Given(/^eu visualizo o campo “Data entrega” com "([^\"]*)"$/, async (dataEntrega) => {
         var idcampo = "labelDataEntrega"
         var campo = document.getElementById("idcampo");
-        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal('10/11/2017');
+        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal(<string> dataEntrega);
     });
 
-    Given(/^eu visualizo o campo “Local de retirada” com "([^\"]*)"$/, async (client) => {
+    Given(/^eu visualizo o campo “Local de retirada” com "([^\"]*)"$/, async (localRetirada) => {
         var idcampo = "inputLocalRetirada"
         var campo = document.getElementById("idcampo");
-        expect($(input[id=“ + idcampo + ”]”).getText()).to.eventually.equal('A peixaria');
+        expect($(input[id=“ + idcampo + ”]”).getText()).to.eventually.equal(<string> localRetirada);
     });
 
-    Given(/^eu visualizo o campo “Desconto” com "([^\"]*)"$/, async (client) => {
+    Given(/^eu visualizo o campo “Desconto” com "([^\"]*)"$/, async (desconto”) => {
         var idcampo = "labelDesconto"
         var campo = document.getElementById("idcampo");
-        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal('15');
+        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal(<string> desconto);
     });
 
-    Given(/^eu visualizo o campo “Tipo de desconto” com "([^\"]*)"$/, async (client) => {
+    Given(/^eu visualizo o campo “Tipo de desconto” com "([^\"]*)"$/, async (tipoDesconto) => {
         var idcampo = "labelTipoDesconto"
         var campo = document.getElementById("idcampo");
-        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal('%');
+        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal(<string> tipoDesconto);
     });
 
-    Given(/^eu visualizo o campo “Condição de pagamento” com "([^\"]*)"$/, async (client) => {
+    Given(/^eu visualizo o campo “Condição de pagamento” com "([^\"]*)"$/, async (condicaoPagamento) => {
         var idcampo = "labelCondiçãoPagamento"
         var campo = document.getElementById("idcampo");
-        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal('À vista');
+        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal(<string> condicaoPagamento);
     });
 
-    Given(/^eu visualizo o campo “Quantidade” com "([^\"]*)"$/, async (client) => {
+    Given(/^eu visualizo o campo “Quantidade” com "([^\"]*)"$/, async (quantidade) => {
         var idcampo = "labelQuantidade"
         var campo = document.getElementById("idcampo");
-        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal('5');
+        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal(<string> quantidade);
 
     });
+
+    Given(/^eu visualizo a label da lista com todos os produtos cadastrados$/, async () => {
+        var idcampo = "labelListaProdutos"
+        var campo = document.getElementById("idcampo");
+        expect($(“label[id=“ + idcampo + ”]”).isPresent());
+    });
+
 
     When(/^eu edito o campo “local de retirada” com "([^\"]*)”$/, async (localRetirada) => {
         await $("input[id='inputLocalRetirada']").sendKeys(<string> localRetirada);
@@ -208,10 +233,10 @@ defineSupportCode(function ({ Given, When, Then }) {
         expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal(<string> desconto);
     });
 
-    Given(/^eu visualizo o campo “Tipo de desconto” com "([^\"]*)"$/, async (TipoDesconto) => {
+    Given(/^eu visualizo o campo “Tipo de desconto” com "([^\"]*)"$/, async (tipoDesconto) => {
         var idcampo = "labelTipoDesconto"
         var campo = document.getElementById("idcampo");
-        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal(<string> TipoDesconto);
+        expect($(“label[id=“ + idcampo + ”]”).getText()).to.eventually.equal(<string> tipoDesconto);
     });
 
     Given(/^eu visualizo o campo “Condição de pagamento” com "([^\"]*)"$/, async (condicaoPagamento) => {
@@ -230,12 +255,12 @@ defineSupportCode(function ({ Given, When, Then }) {
     Given(/^eu visualizo o botão de marcar como cancelado$/, async () => {
         var idElem = "botaoCancelar"
         var elem = document.getElementById("idElem");
-        expect($(“button[id=“ + idElem+ ”]”).getText()).to.eventually.equal('botaoCancelar');
+        expect($(“input[id=“ + idElem+ ”]”).getText()).to.eventually.equal('botaoCancelar');
     });
 
 
     When(/^eu clico no botão de marcar como cancelado$/, async () => {
-       await element(by.buttonText('“Cancelado”')).click();
+       await element(by.inputText('“Cancelado”')).click();
     });
 
     Then(/^o pedido estara marcado como cancelado$/, async () => {
@@ -244,3 +269,4 @@ defineSupportCode(function ({ Given, When, Then }) {
         expect($(“label[id=“ + idLabel + ”]”).isPresent());
     });
 })
+    
