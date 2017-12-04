db8656ee4d76149d47fa00b53350321607da03f8
Alecsandros <Ascj@cin.ufpe.br>
2017-12-04 10:12:54 -0300

    [#153208932] Implementação do calculo de peso dos produtos de contabilidade

---
+++ gui/ap-gui/src/app/contabilidade.component.ts
@@ -0,0 +1,93 @@
+import { Component, OnInit } from '@angular/core'; 
+import { NgModule } from '@angular/core';
+ 
+import { Contabilidade } from './contabilidade';
+import { ContabilidadeService } from './contabilidade.service';
+import { RegistroProdutoComponent} from '/.registroproduto.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './contabilidade.component.html',
+  styleUrls: ['./contabilidade.component.css']
+})
+
+export class ContabilidadeComponent implements OnInit {
+	constructor(private contabilidadeService: ContabilidadeService) {}
+	contabilidade: Contabilidade = new Contabilidade();
+	lucro: decimal = 0;
+	bruto: decimal = 0;
+
+	contabilidadePedido(pedido: Pedido){ //ver isto
+		var listaDeCompra = pedido.Lista; 
+		for (let i in listaDeCompra){
+			var itemDeCompra = listaDeCompra[i];
+			var produto = itemDeCompra.produto;
+			var indice = ProcurarProduto(produto);
+			var preco = produto.Valor;
+			var quantidade = itemDeCompra.quantidade;
+			var desconto = pedido.Desconto;
+			var bruto = preco * quantidade;
+			var lucro = (bruto/100) * desconto;
+			incrementarValorProduto(bruto, quantidade, indice);
+			incrementarValor(lucro, bruto);
+		}
+		this.contabilidadeService.atualizar(this.contabilidade);
+	}
+	
+	diario(): void {
+		this.lucro = this.contabilidade.lucroDiario;
+		this.bruto = this.contabilidade.brutoDiario;
+		for(let i of ArrayProduto){
+			i.bruto = i.brutoDiario;
+			i.peso = i.pesoDiario;
+		}
+  	}
+
+  	semanal(): void {
+		this.lucro = this.contabilidade.lucroSemanal;
+		this.bruto = this.contabilidade.brutoSemanal;
+		for(let i of ArrayProduto){
+			i.bruto = i.brutoSemanal;
+			i.peso = i.pesoSemanal;
+		}
+  	}
+
+  	mensal(): void {
+		this.lucro = this.contabilidade.lucroMensal;
+		this.bruto = this.contabilidade.brutoMensal;
+		for(let i of ArrayProduto){
+			i.bruto = i.brutoMensal;
+			i.peso = i.pesoMensal;
+		}
+  	}
+
+	incrementarValor(lucro: decimal, bruto: decimal){ //V1
+		contabilidade.lucroDiario +=  lucro;
+		contabilidade.lucroSemanal += lucro;
+		contabilidade.lucroMensal += lucro;
+		contabilidade.brutoDiario += bruto;
+		contabilidade.brutoSemanal += bruto;
+		contabilidade.brutoMensal += bruto;
+	}
+
+	incrementarValorProduto(bruto: decimal, peso: decimal, indice: integer){ //V1
+		var prod: RegistroProduto = this.contabilidade.ArrayProduto[indice];
+		prod.brutoDiario += bruto;
+		prod.brutoSemanal += bruto;
+		prod.brutoMensal += bruto;
+		prod.pesoDiario += peso;
+		prod.pesoSemanal += peso;
+		prod.pesoMensal += peso;
+	}
+
+	ngOnInit(): void {
+	    console.log();
+	    this.contabilidadeService.getContabilidade();
+	        .then(cont => this.contabilidade = cont);
+	        .catch(erro => alert(erro));
+    }
+
+    onMove(): void {
+    }
+}
+
---
+++ gui/ap-gui/src/app/registroproduto.ts
@@ -0,0 +1,54 @@
+import {Produto} from './produto';
+import {Pedido} from './pedido';
+
+export class RegistroProduto{
+		produto: Produto;
+		bruto: decimal;
+		brutoDiario: decimal;
+		brutoSemanal: decimal; 
+		brutoMensal: decimal;
+		peso: decimal;
+		pesoDiario: decimal; 
+		pesoSemanal: decimal;
+		pesoMensal: decimal;
+
+	constructor(){
+		this.clean();
+	}
+
+	clean() = void{
+		this.produto = new Produto();
+		this.bruto = 0
+		this.brutoDiario = 0 
+		this.brutoSemanal = 0 
+		this.brutoMensal = 0
+		this.peso = 0
+		this.pesoDiario = 0 
+		this.pesoSemanal = 0 
+		this.pesoMensal = 0
+	}
+
+	clone(): RegistroProduto {
+		var registroproduto: RegistroProduto = new RegistroProduto();
+		produto:copyFrom(this);
+		return item;
+	}
+
+	copyFrom(from: RegistroProduto): void {
+		this.copyProdutoFrom(from.produto); 
+		this.bruto = from.bruto;
+		this.brutoDiario = from.brutoDiario;
+		this.brutoSemanal = from.brutoSemanal;
+		this.brutoMensal = from.brutoMensal;
+		this.peso = from.peso;
+		this.pesoDiario = from.pesoDiario;
+		this.pesoSemanal = from.pesoSemanal;
+		this.pesoMensal = from.pesoMensal;
+	}
+
+	copyProdutoFrom(from: Produto): void{
+		this.produto = new Produto();
+		this.produto = from;
+	}
+}
+
