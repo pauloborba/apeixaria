7f34776b008b9d767a1e9062df1c319d86c29a87
Alecsandros <Ascj@cin.ufpe.br>
2017-12-07 21:24:30 -0300

    [#153208932] Criação da do cenário e teste de aceitação

--- tests-acceptance/stepdefinitions/contabilidade.ts
+++ tests-acceptance/stepdefinitions/contabilidade.ts
@@ -11,32 +11,32 @@ defineSupportCode(function ({ Given, When, Then }) {
     Given(/^estou na página de Contabilidade $/, async () => {
         await browser.get("http://localhost:4200/");
         await expect(browser.getTitle()).to.eventually.equal('ApGui');
-        await $("a[name='Produtos Pendentes']").click();
+        await $("a[name='Contabilidade']").click();
     })
 
-    Given(/^o valor atual do lucro e peso do "([^\"]*)" é R$ "([^\"]*)" e "([^\"]*)" Kg respectivamente $/, async (produto, valor, peso) => {
-        //Cadastrar o produto e verificar
-    });
-
-    Given(/^o pedido "(\d*)" cadastrado ao cliente "([^\"]*)" com "(\d*)" unidades de "([^\"]*)" possui um valor bruto de R$ "([^\"]*)"$/, async (code, client, quant, produto,preco) => {
+   Given(/^o pedido "(\d*)" cadastrado ao cliente "([^\"]*)" com "(\d*)" unidades de "([^\"]*)" possui um valor bruto de R$ "([^\"]*)"$/, async (code, client, quant, produto,preco) => {
         var bruto: number= (Number(preco)*Number(quant));
         var options:any = {method: 'POST', uri: ("http://localhost:3000/pedidos"), body:{"cliente": {"nome": client}, "entregue" : true, "pago": true, "lista":[{"produto":{"codigo":"3", "nome":produto, "valor":preco},"quantidade":quant,"valor":bruto}]}, json: true};
         request(options);
         request.post("localhost:3000/pedidos");
     });
 
-    When(/^eu selecionar a opção Contabilidade % $/, async () => {
-        await $("button#Contabilidade").click();
+   Given(/^o pedido "(\d*)" cadastrado ao cliente "([^\"]*)" com "(\d*)" unidades de "([^\"]*)" possui um valor bruto de R$ "([^\"]*)"$/, async (code, client, quant, produto,preco) => {
+        var bruto: number= (Number(preco)*Number(quant));
+        var options:any = {method: 'POST', uri: ("http://localhost:3000/pedidos"), body:{"cliente": {"nome": client}, "entregue" : true, "pago": true, "lista":[{"produto":{"codigo":"3", "nome":produto, "valor":preco},"quantidade":quant,"valor":bruto}]}, json: true};
+        request(options);
+        request.post("localhost:3000/pedidos");
     });
 
-    Then(/^ Eu verei no campo Peso do produto "([^\"]*)" o valor "([^\"]*)" KG $/, async (produto, peso) => {
+   Given(/^o pedido "(\d*)" cadastrado ao cliente "([^\"]*)" com "(\d*)" unidades de "([^\"]*)" possui um valor bruto de R$ "([^\"]*)"$/, async (code, client, quant, produto,preco) => {
+        var bruto: number= (Number(preco)*Number(quant));
+        var options:any = {method: 'POST', uri: ("http://localhost:3000/pedidos"), body:{"cliente": {"nome": client}, "entregue" : true, "pago": true, "lista":[{"produto":{"codigo":"3", "nome":produto, "valor":preco},"quantidade":quant,"valor":bruto}]}, json: true};
+        request(options);
+        request.post("localhost:3000/pedidos");
+    });
+
+    Then(/^ Eu verei no campo peso do produto "([^\"]*)" o peso "([^\"]*)" KG $/, async (produto, valor) => {
         //verificar o campo de peso
-        /*expect(await element(by.css(`table#${cat}-table tbody tr#id-${cod}`)).isPresent()).to.be.true;*/
-    });
-
-    Then(/^ Eu verei no campo lucro do produto "([^\"]*)" o valor "([^\"]*)" KG $/, async (produto, valor) => {
-        //verificar o campo de lucro
-        /*expect(await element(by.css(`table#${cat}-table tbody tr#id-${cod}`)).isPresent()).to.be.true;*/
     });
 
 }) 
